9ad73794a7c41843989efbb033848137b552f677
JoeBulley <bulleyyy@gmail.com>
2021-11-08 12:37:08 +1100

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback) {
+    callback();
+
+}
+
+function returnsANamedFunction() {
+    const namedFunction = function() {}
+
+    return namedFunction;
+
+}
+
+function returnsAnAnonymousFunction() {
+
+return function(){}
+}
